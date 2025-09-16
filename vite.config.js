208d8cf5eecf7cadb37e8371bbd301fb8734aabf
AllequiZ <henriqueamc2003@gmail.com>
2025-09-15 22:19:42 -0300

    configurando .yaml

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
 
-  base: process.env.NODE_ENV === 'production' ? '/computascript/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/AR-Cave-Immersion/' : '',
 
   plugins: [
     basicSsl()
